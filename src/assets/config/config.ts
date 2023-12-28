eb32f36962c204aae79a835aa118bb413d8bc5e7
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2023-12-28 15:31:56 +0200

    Set up granska configuration

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -2,127 +2,72 @@ type Config = { [key: string]: any }
 
 export const config: Config = {
   app: {
-    siteURLOrigin: "https://topelius.sls.fi",
-    projectNameDB: "topelius",
-    projectId: 10,
-    backendBaseURL: "https://testa-vonwright.sls.fi:8000/digitaledition",
-    alternateFacsimileBaseURL: "",
+    siteURLOrigin: "https://granska-historiskarecept.sls.fi",
+    projectNameDB: "historiskarecept",
+    projectId: 11,
+    backendBaseURL: "https://granska-api.sls.fi/digitaledition",
     i18n: {
       languages: [
-        { code: "sv", label: "Svenska", region: "FI" },
-        { code: "fi", label: "Suomi", region: "FI" }
+        { code: "sv", label: "Svenska", region: "FI" }
       ],
-      defaultLanguage: "sv",
-      multilingualCollectionTableOfContents: false,
-      multilingualReadingTextLanguages: [],
-      multilingualNamedEntityData: false
+      defaultLanguage: "sv"
     },
     enableRouterLoadingBar: true,
     openGraphMetaTags: {
       enabled: true,
       image: {
         sv: {
-          altText: "alt-text",
-          URL: "assets/images/home-page-banner.jpg"
-        },
-        fi: {
-          altText: "alt-teksti",
-          URL: "assets/images/home-page-banner.jpg"
+          altText: "Fotografi av ett fat med råa grönsaker och en hel fisk med en kniv instucken vertikalt. Foto: Katja Hagelstam.",
+          URL: "assets/images/open-graph/historiska-recept-stilleben-Katja-Hagelstam.jpg"
         }
       }
     }
   },
   collections: {
-    addTEIClassNames: true,
-    replaceImageAssetsPaths: true,
-    enableLegacyIDs: true,
+    addTEIClassNames: false,
+    replaceImageAssetsPaths: false,
+    enableLegacyIDs: false,
     enableMathJax: false,
-    firstTextItem: {
-      216: "216_20280", 219: "219_19443", 220: "220_20122",
-      218: "218_20230_ch2", 210: "210_20548_ch1", 208: "208_18466_ch4",
-      207: "207_18464_ch1", 214: "214_20240_ch1", 203: "203_20217_ch1",
-      213: "213_18465_ch1", 202: "202_18467_ch1", 199: "199_18284",
-      221: "221_21422", 206: "206_20212_ch1", 201: "201_18471",
-      211: "211_20128", 200: "200_19870", 205: "205_20227_ch1",
-      215: "215_20568", 217: "217_20559_ch1", 204: "204_20322",
-      212: "212_20323", 209: "209_20479"
-    },
     frontMatterPages: {
-      cover: true,
-      title: true,
-      foreword: true,
+      cover: false,
+      title: false,
+      foreword: false,
       introduction: true
     },
     highlightSearchMatches: true,
-    inlineIllustrations: [206],
-    mediaCollectionMappings: { 214: 44, 206: 19, 218: 19 },
     order: [
-      [216, 219, 220, 218, 210, 208, 207, 214, 203, 213,
-        202, 199, 221, 206, 201, 211, 200, 205, 215, 217,
-        204, 212, 209]
+      [541, 2560, 1088, 812, 1089, 1367, 2561, 2562, 2563, 2564, 2565]
     ]
   },
   ebooks: [
     {
-      title: "Bröd och bot",
+      title: "Om hushållsböcker och receptsamlingar under 1700-talet",
       filename: "norrback-brod-och-bot.epub",
       externalFileURL: "",
-      coverURL: "",
+      coverURL: "assets/images/covers/cover_norrback-brod-och-bot_epub.jpg",
       downloadOptions: [
         {
-          url: "https://www.sls.fi/sv/utgivning/historiska-recept",
+          url: "https://urn.fi/URN:ISBN:978-951-583-582-6",
           label: ""
-        }
-      ]
-    },
-    {
-      title: "Marriage Conditions in a Palestinian Village I (epub)",
-      filename: "marriage-conditions-1.epub",
-      externalFileURL: "https://api.sls.fi/digitaledition/granqvist/files/30/epub/30_11672_Marriage_Conditions_1.epub/",
-      coverURL: "",
-      downloadOptions: [
-        {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/epub/30_11672_Marriage_Conditions_1.epub/",
-          label: "EPUB"
-        },
-        {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/pdf/30_11672_Marriage_Conditions_1.pdf/",
-          label: "PDF"
-        }
-      ]
-    },
-    {
-      title: "Marriage Conditions in a Palestinian Village I (pdf)",
-      filename: "marriage-conditions-1.pdf",
-      externalFileURL: "https://api.sls.fi/digitaledition/granqvist/files/30/pdf/30_11672_Marriage_Conditions_1.pdf/",
-      coverURL: "",
-      downloadOptions: [
-        {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/epub/30_11672_Marriage_Conditions_1.epub/",
-          label: "EPUB"
-        },
-        {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/pdf/30_11672_Marriage_Conditions_1.pdf/",
-          label: "PDF"
         }
       ]
     }
   ],
   page: {
     about: {
-      initialPageNode: "01-01"
+      initialPageNode: "01"
     },
     elasticSearch: {
       enableFilters: true,
-      enableSortOptions: true,
-      filterGroupsOpenByDefault: ["Years", "Type", "Genre", "Collection"],
+      enableSortOptions: false,
+      filterGroupsOpenByDefault: ["Type", "Collection"],
       hitsPerPage: 15,
-      indices: ["topelius"],
+      indices: ["historiskarecept"],
       openReadingTextWithComments: false,
       textHighlightFragmentSize: 150,
       textHighlightType: "fvh",
       textTitleHighlightType: "fvh",
-      typeFilterGroupOptions: ["est", "com", "var", "inl", "tit", "fore"],
+      typeFilterGroupOptions: ["est", "ms", "inl"],
       fixedFilters: [
         {
           terms: {
@@ -137,23 +82,9 @@ export const config: Config = {
       ],
       additionalSourceFields: [],
       aggregations: {
-        Years: {
-          date_histogram: {
-            field: "orig_date_sort",
-            calendar_interval: "year",
-            format: "yyyy"
-          }
-        },
         Type: {
           terms: {
             field: "text_type",
-            size: 40,
-            order: {_key: "asc"}
-          }
-        },
-        Genre: {
-          terms: {
-            field: "publication_data.genre.keyword",
             size: 40,
             order: {_key: "asc"}
           }
@@ -164,42 +95,13 @@ export const config: Config = {
             size: 40,
             order: {_key: "asc"}
           }
-        },
-        LetterSenderName: {
-          terms: {
-            field: "sender_subject_name.keyword",
-            size: 100
-          }
-        },
-        LetterReceiverName: {
-          terms: {
-            field: "receiver_subject_name.keyword",
-            size: 100
-          }
-        },
-        LetterSenderLocation: {
-          terms: {
-            field: "sender_location_name.keyword",
-            size: 50
-          }
-        },
-        LetterReceiverLocation: {
-          terms: {
-            field: "receiver_location_name.keyword",
-            size: 50
-          }
         }
       }
-    },
-    foreword: {
-      showURNButton: true,
-      showViewOptionsButton: true
     },
     home: {
       bannerImage: {
         altTexts: {
-          sv: "Porträtt av Zacharias Topelius",
-          fi: "Zacharias Topeliuksen muotokuva"
+          sv: "Fotografi av ett fat med råa grönsaker och en hel fisk med en kniv instucken vertikalt. Bakom tallriken finns en guldramad målning av en äldre man framför ett fat med fisk. Allt är arrangerat på ett träbord framför en mörk bakgrund. Foto: Katja Hagelstam."
         },
         orientationPortrait: true,
         URL: "assets/images/home-page-banner-portrait.jpg"
@@ -207,103 +109,69 @@ export const config: Config = {
       portraitOrientationSettings: {
         imagePlacement: {
           onRight: false,
-          squareCroppedVerticalOffset: "10%"
+          squareCroppedVerticalOffset: "27%"
         },
-        siteTitleOnImageOnSmallScreens: false
+        siteTitleOnImageOnSmallScreens: true
       },
-      showContentGrid: false,
+      showContentGrid: true,
       showFooter: true,
       showSearchbar: false
     },
     index: {
       keywords: {
         maxFetchSize: 500,
-        showFilter: true,
-        publishedStatus: 2
-      },
-      persons: {
-        database: "elastic",
-        maxFetchSize: 500,
-        showFilter: true,
-        publishedStatus: 2
-      },
-      places: {
-        maxFetchSize: 500,
-        showFilter: true,
-        publishedStatus: 2
-      },
-      works: {
+        showFilter: false,
         publishedStatus: 2
       }
     },
     introduction: {
-      hasSeparateTOC: true,
+      hasSeparateTOC: false,
       showTextDownloadButton: true,
       showURNButton: true,
       showViewOptionsButton: true,
       viewOptions: {
-        personInfo: true,
+        personInfo: false,
         placeInfo: false,
-        workInfo: true,
+        workInfo: false,
         paragraphNumbering: true,
-        pageBreakEdition: true
+        pageBreakEdition: false
       }
     },
-    mediaCollection: {
-      showURNButton: true
-    },
     text: {
-      defaultViews: ["readingtext", "comments", "facsimiles"],
-      defaultViewOptions: ["comments"],
+      defaultViews: ["readingtext", "legend", "facsimiles", "manuscripts"],
+      defaultViewOptions: [],
       showTextDownloadButton: true,
       showURNButton: true,
       showViewOptionsButton: true,
       viewOptions: {
-        comments: true,
-        personInfo: true,
-        placeInfo: true,
-        emendations: true,
-        normalisations: true,
-        workInfo: true,
-        abbreviations: true,
+        comments: false,
+        personInfo: false,
+        placeInfo: false,
+        emendations: false,
+        normalisations: false,
+        workInfo: false,
+        abbreviations: false,
         paragraphNumbering: true,
         pageBreakOriginal: true,
-        pageBreakEdition: true
+        pageBreakEdition: false
       },
       viewTypes: {
         showAll: true,
         readingtext: true,
-        comments: true,
+        comments: false,
         facsimiles: true,
         manuscripts: true,
-        variants: true,
-        illustrations: true,
+        variants: false,
+        illustrations: false,
         legend: true,
         metadata: false
       }
-    },
-    title: {
-      loadContentFromMarkdown: false,
-      showURNButton: true,
-      showViewOptionsButton: true
     }
   },
   component: {
-    collectionSideMenu: {
-      sortableCollectionsAlphabetical: ["211", "215", "219", "220"],
-      sortableCollectionsChronological: ["215", "219", "220"],
-      sortableCollectionsCategorical: [],
-      categoricalSortingPrimaryKey: "",
-      categoricalSortingSecondaryKey: ""
-    },
     contentGrid: {
-      includeEbooks: false,
+      includeEbooks: true,
       includeMediaCollection: false,
-      mediaCollectionCoverURL: "",
-      mediaCollectionCoverAltTexts: {
-        sv: "Alt-text",
-        fi: "Alt-teksti"
-      },
       showTitles: true
     },
     epub: {
@@ -321,37 +189,34 @@ export const config: Config = {
         about: true,
         ebooks: true,
         collections: true,
-        mediaCollections: true,
-        indexKeywords: false,
-        indexPersons: true,
-        indexPlaces: true,
+        mediaCollections: false,
+        indexKeywords: true,
+        indexPersons: false,
+        indexPlaces: false,
         indexWorks: false
       }
     },
     manuscripts: {
-      showTitle: true,
-      showNormalizedToggle: true,
-      showOpenLegendButton: true
+      showTitle: false,
+      showNormalizedToggle: false,
+      showOpenLegendButton: false
     },
     topMenu: {
       showAboutButton: true,
       showContentButton: true,
       showElasticSearchButton: true,
       showURNButton: false,
-      showLanguageButton: true,
+      showLanguageButton: false,
       showSiteLogo: true,
       siteLogoDefaultImageURL: "assets/images/logo.svg",
       siteLogoMobileImageURL: "assets/images/logo-mobile.svg",
       siteLogoLinkURL: "https://www.sls.fi/"
-    },
-    variants: {
-      showOpenLegendButton: true
     }
   },
   modal: {
     downloadTexts: {
       introductionFormats: {
-        xml: true,
+        xml: false,
         html: false,
         xhtml: false,
         txt: false,
@@ -365,14 +230,14 @@ export const config: Config = {
         print: true
       },
       commentsFormats: {
-        xml: true,
+        xml: false,
         html: false,
         xhtml: false,
         txt: false,
-        print: true
+        print: false
       },
       manuscriptsFormats: {
-        xml: false,
+        xml: true,
         html: false,
         xhtml: false,
         txt: false,
@@ -384,18 +249,6 @@ export const config: Config = {
     },
     referenceData: {
       URNResolverURL: "https://urn.fi/",
-    },
-    namedEntity: {
-      showAliasAndPrevLastName: false,
-      showArticleData: false,
-      showCityRegionCountry: false,
-      showDescriptionLabel: false,
-      showGalleryOccurrences: false,
-      showMediaData: false,
-      showOccupation: false,
-      showOccurrences: true,
-      showType: false,
-      useSimpleWorkMetadata: true
     }
   }
 }
